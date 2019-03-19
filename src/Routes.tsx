308c8b40a67d286dfebf05450d0ab14321dd48bb
Ricky <Ricky.Lu@yungtay.com.tw>
2019-03-19 12:54:34 +0800

    ⬆️ react-router-dom 4.4.0 -> 5.0.0 ➖ history

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Router } from 'react-router';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
-import history from './history';
 
 import Login from './screens/Login';
 import Layout from './layouts';
 
 const Routes = () => (
-  <Router history={history}>
+  <Router>
     <Switch>
       {/* public */}
       <Route exact path="/login" component={Login} />
